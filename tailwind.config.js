b7f678b15bfbe74a128aa073426e23532f8a8f77
taeio <176225411+taeio@users.noreply.github.com>
2025-10-25 09:29:25 -0700

    fixing npm/nvm.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "src/pages/**/*.{js,ts,jsx,tsx}",
-    "src/components/**/*.{js,ts,jsx,tsx}",
-    "src/app/**/*.{js,ts,jsx,tsx}",
-  ],
-  /** @type {import('tailwindcss').Config} */
-module,exports : {
-  content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        columbia: {
-          700: "#4A90E2",
-          900: "#2C5DAA",
-        },
-        silver: "#C0C0C0",
-        red: {
-          DEFAULT: "#E63946",
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
-},
-}
+};
+
